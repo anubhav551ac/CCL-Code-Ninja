f812519b48161729a528004e69dcb9bcc352349f
Anubhav Adhikari <anubhavadhikari2008@gmail.com>
2026-03-06 23:23:04 +0545

    debugged

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require("express");
+
+const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "frontend")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
